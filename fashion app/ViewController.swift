da73365a5e0142d0352f4c3c1dd5d4e02d2c146c
Prashant Deshpande <p.deshpande@gmail.com>
2022-07-19 13:54:27 -0700

    Initial Commit

---
+++ fashion app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fashion app
+//
+//  Created by Prashant Deshpande on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var imageOne: UIImageView!
+    
+}
+
